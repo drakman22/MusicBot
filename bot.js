3e7d367d40ab521c4b4d33872983d9bdf985dd55
drakman22 <86939466+drakman22@users.noreply.github.com>
2021-07-13 06:51:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -412,4 +412,4 @@ function play(guild, song){
                 .setColor([226, 50, 41])
             return serverQueue.textChannel.sendEmbed(embedfunction1);
 }
-client.login(process.env.BOT_TOKEN);
+client.login("ODM4NjI0NDQwMzE2NDYxMDg2.YI9z3w.BgOHuMgcu-ZUdpyzvWQTAAEFjMI";
